d68f4e06bf8d07990aa90bb90aa275963fce8d13
Tuấn Minh <tuanminh@192.168.1.6>
2026-07-14 16:39:05 +0700

    fix: Hardcode version in Info.plist for Xcode Cloud App Store Connect Validation

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -16,8 +16,14 @@ flutter precache --ios
 # Install Flutter dependencies.
 flutter pub get
 
-# Generate Generated.xcconfig for Xcode Cloud
-flutter build ios --release --config-only
+# Hardcode version in Info.plist for Xcode Cloud App Store Connect Validation
+VERSION=$(grep '^version: ' pubspec.yaml | sed 's/version: //')
+VERSION_NAME=$(echo $VERSION | cut -d '+' -f 1)
+VERSION_NUMBER=$(echo $VERSION | cut -d '+' -f 2)
+
+echo "Setting version to $VERSION_NAME ($VERSION_NUMBER) in Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VERSION_NAME" ios/Runner/Info.plist || /usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string $VERSION_NAME" ios/Runner/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $VERSION_NUMBER" ios/Runner/Info.plist || /usr/libexec/PlistBuddy -c "Add :CFBundleVersion string $VERSION_NUMBER" ios/Runner/Info.plist
 
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
